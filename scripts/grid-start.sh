2b022b4ee22b4b8de298c6d4ec6ee354026a5f5a
dkwasny <dkwasny@centos-1>
2013-11-26 23:50:35 -0500

    Adding the iptables shutdown step of the grid start script...im lazy.  Adding some sarah printlns as well.

--- scripts/grid-start.sh
+++ scripts/grid-start.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
+SERVERS=$(cat $HADOOP_CONF_DIR/slaves);
+echo "I am a bad person...stopping iptables on the grid...actually setup the ports when you are not lazy!!";
+for SERVER in $SERVERS; do
+	ssh root@$SERVER service iptables stop;
+done;
+
 $HADOOP_COMMON_HOME/sbin/start-dfs.sh;
 $HADOOP_COMMON_HOME/sbin/start-yarn.sh;
 $HADOOP_COMMON_HOME/sbin/mr-jobhistory-daemon.sh start historyserver;
 
-for SERVER in $(cat $HADOOP_CONF_DIR/slaves); do
+for SERVER in $SERVERS; do
+	echo "Starting Zookeeper on $SERVER";
 	ssh $SERVER "\
 		JMXDISABLE=INOWANTJMX \
 		SERVER_JVMFLAGS='-Xmx128m' \
--- scripts/grid-stop.sh
+++ scripts/grid-stop.sh
@@ -5,6 +5,7 @@ $1/sbin/stop-yarn.sh;
 $1/sbin/mr-jobhistory-daemon.sh stop historyserver;
 
 for SERVER in $(cat $HADOOP_CONF_DIR/slaves); do
+	echo "Stopping Zookeeper on $SERVER";
 	ssh $SERVER "\
 		JMXDISABLE=INOWANTJMX \
 		SERVER_JVMFLAGS='-Xmx128m' \
