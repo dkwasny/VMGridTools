077dd6fa1ff2d3a516df4bd2b156f3bc6795f24e
dkwasny <dkwasny@centos-1>
2013-11-26 23:38:05 -0500

    Final touches for the new script config layout

--- scripts/grid-start.sh
+++ scripts/grid-start.sh
@@ -5,5 +5,9 @@ $HADOOP_COMMON_HOME/sbin/start-yarn.sh;
 $HADOOP_COMMON_HOME/sbin/mr-jobhistory-daemon.sh start historyserver;
 
 for SERVER in $(cat $HADOOP_CONF_DIR/slaves); do
-	ssh $SERVER "JMXDISABLE=INOWANTJMX SERVER_JVMFLAGS='-Xmx128m' $ZOOKEEPER_HOME/bin/zkServer.sh start $HADOOP_CONF_DIR/zookeeper.properties";
+	ssh $SERVER "\
+		JMXDISABLE=INOWANTJMX \
+		SERVER_JVMFLAGS='-Xmx128m' \
+		$ZOOKEEPER_HOME/bin/zkServer.sh start $HADOOP_CONF_DIR/zookeeper.properties \
+	";
 done;
--- scripts/grid-stop.sh
+++ scripts/grid-stop.sh
@@ -3,3 +3,11 @@
 $1/sbin/stop-dfs.sh;
 $1/sbin/stop-yarn.sh;
 $1/sbin/mr-jobhistory-daemon.sh stop historyserver;
+
+for SERVER in $(cat $HADOOP_CONF_DIR/slaves); do
+	ssh $SERVER "\
+		JMXDISABLE=INOWANTJMX \
+		SERVER_JVMFLAGS='-Xmx128m' \
+		$ZOOKEEPER_HOME/bin/zkServer.sh stop $HADOOP_CONF_DIR/zookeeper.properties \
+	";
+done;
