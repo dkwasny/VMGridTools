d301a140231b50efed147ad9942ddc1c8f94befa
dkwasny <dkwasny67@gmail.com>
2014-06-06 17:39:48 -0400

    Aliases and non interactive shells don't play nice.

--- scripts/tomcat-grid-toggle.sh
+++ scripts/tomcat-grid-toggle.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
 TOGGLE="$1";
-if [ "$TOGGLE" != "stop" ] || [ "$TOGGLE" != "start"]; then
+if [ "$TOGGLE" == "stop" ]; then
+	CMD="$CATALINA_HOME/bin/shutdown.sh";
+elif [ "$TOGGLE" == "start" ]; then
+	CMD="$CATALINA_HOME/bin/startup.sh";
+else
 	echo "bad command ($TOGGLE). Use stop or start.";
 	exit 1;
 fi;
 
 for i in 1 2 3 4; do
-	ssh centos-$i "tomcat-$TOGGLE";
+	ssh centos-$i "$CMD";
 done;
